8abbc713109bf862593d07cd4f923ae97e427c8b
deruss <deruss79@hotmail.com>
2013-09-27 11:50:54 +1100

    removed a blank line

--- PayPalMobileForXamarin/libPayPalMobile.linkwith.cs
+++ PayPalMobileForXamarin/libPayPalMobile.linkwith.cs
@@ -3,7 +3,6 @@ using MonoTouch.ObjCRuntime;
 
 [assembly: LinkWith ("libPayPalMobile.a",
                      LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator,
-
                      ForceLoad = true,
                      Frameworks="AVFoundation CoreMedia CoreVideo SystemConfiguration Security MessageUI OpenGLES MobileCoreServices",
                      LinkerFlags="-lz -lxml2 -lc++"
