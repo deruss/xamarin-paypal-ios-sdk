b485d3095d06f052b9277792830cd213f8da8d16
deruss <deruss79@hotmail.com>
2013-09-27 11:42:29 +1100

    Fixed LinkWih options to work on devices.

--- PayPalMobileForXamarin/libPayPalMobile.linkwith.cs
+++ PayPalMobileForXamarin/libPayPalMobile.linkwith.cs
@@ -1,4 +1,10 @@
 using System;
 using MonoTouch.ObjCRuntime;
 
-[assembly: LinkWith ("libPayPalMobile.a", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator, ForceLoad = true, LinkerFlags="-lz -lxml2 -lstdc++")]
+[assembly: LinkWith ("libPayPalMobile.a",
+                     LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator,
+
+                     ForceLoad = true,
+                     Frameworks="AVFoundation CoreMedia CoreVideo SystemConfiguration Security MessageUI OpenGLES MobileCoreServices",
+                     LinkerFlags="-lz -lxml2 -lc++"
+                     )]
